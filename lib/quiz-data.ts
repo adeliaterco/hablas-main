83837486824f4142b2da898708be4bc342989d01
Mariaclara728 <amandateixeiramakeup@gmail.com>
2025-06-13 12:01:09 -0300

    gfgfg

--- lib/quiz-data.ts
+++ lib/quiz-data.ts
@@ -324,6 +324,7 @@ export const testimonials = [
 export const socialProofMessages = [
   "¡Estás entre el 17% más determinado a reconquistar!",
   "¡Tu perfil muestra compatibilidad!",
+  "¡Bonificación liberada por desbloqueo!",
   "¡Has desbloqueado los 2 bonos - valor total de $84!",
   "El 87% de las personas en tu situación lograron resultados en menos de 14 días",
   "Estás más comprometido que el 73% de las personas que hicieron esta prueba",
